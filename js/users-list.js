2d4a426ddda2a531307629dc3f6fe9c9eaed4b49
edukhanci <tutoresedukhancompazibura@gmail.com>
2024-10-14 11:28:54 -0300

    Add files via upload

--- js/users-list.js
+++ js/users-list.js
@@ -5,7 +5,7 @@ const student = [
 		//nome do usuario (username)
 		'akassiasilva',
 		//evolução da pontuação
-		[ ['2024-09-26', 115271], ['2024-10-01', 115271], ['2024-10-03', 118016], ['2024-10-08', 120846] ],
+		[ ['2024-09-26', 115271], ['2024-10-01', 115271], ['2024-10-03', 118016], ['2024-10-08', 120846], ['2024-10-10', 120846] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -13,7 +13,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/stelly-blue.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 0], ['2024-10-03', 1], ['2024-10-08', 1] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 0], ['2024-10-03', 1], ['2024-10-08', 1], ['2024-10-10', 0] ]
 	],
 	[
 		//nome de exibição
@@ -21,7 +21,7 @@ const student = [
 		//nome do usuario (username)
 		'emanuelnicolas',
 		//evolução da pontuação
-		[ ['2024-09-26', 14965], ['2024-10-01', 25975], ['2024-10-03', 27440], ['2024-10-08', 42650] ],
+		[ ['2024-09-26', 14965], ['2024-10-01', 25975], ['2024-10-03', 27440], ['2024-10-08', 42650], ['2024-10-10', 42650] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -29,7 +29,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1], ['2024-10-10', 0] ]
 	],
 	[
 		//nome de exibição
@@ -37,7 +37,7 @@ const student = [
 		//nome do usuario (username)
 		'fabriciosilva',
 		//evolução da pontuação
-		[ ['2024-09-26', 26217], ['2024-10-01', 26217], ['2024-10-03', 26217], ['2024-10-08', 26217] ],
+		[ ['2024-09-26', 26217], ['2024-10-01', 26217], ['2024-10-03', 26217], ['2024-10-08', 26217], ['2024-10-10', 26217] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -45,7 +45,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/eggleston-orange.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 0], ['2024-10-01', 0], ['2024-10-03', 0], ['2024-10-08', 0] ]
+		[ ['2024-09-26', 0], ['2024-10-01', 0], ['2024-10-03', 0], ['2024-10-08', 0], ['2024-10-10', 0] ]
 	],
 	[
 		//nome de exibição
@@ -53,7 +53,7 @@ const student = [
 		//nome do usuario (username)
 		'fagnnersilva',
 		//evolução da pontuação
-		[ ['2024-09-26', 60410], ['2024-10-01', 60410], ['2024-10-03', 60410], ['2024-10-08', 60410] ],
+		[ ['2024-09-26', 60410], ['2024-10-01', 60410], ['2024-10-03', 60410], ['2024-10-08', 60410], ['2024-10-10', 60410] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -61,7 +61,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 0], ['2024-10-01', 0], ['2024-10-03', 0], ['2024-10-08', 0] ]
+		[ ['2024-09-26', 0], ['2024-10-01', 0], ['2024-10-03', 0], ['2024-10-08', 0], ['2024-10-10', 0] ]
 	],
 	[
 		//nome de exibição
@@ -69,7 +69,7 @@ const student = [
 		//nome do usuario (username)
 		'grazielisilva',
 		//evolução da pontuação
-		[ ['2024-09-26', 94007], ['2024-10-01', 97842], ['2024-10-03', 100407], ['2024-10-08', 101588] ],
+		[ ['2024-09-26', 94007], ['2024-10-01', 97842], ['2024-10-03', 100407], ['2024-10-08', 101588], ['2024-10-10', 103073] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -77,7 +77,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1], ['2024-10-10', 1] ]
 	],
 	[
 		//nome de exibição
@@ -85,7 +85,7 @@ const student = [
 		//nome do usuario (username)
 		'luizsantos',
 		//evolução da pontuação
-		[ ['2024-09-26', 91411], ['2024-10-01', 91411], ['2024-10-03', 91411], ['2024-10-08', 91411] ],
+		[ ['2024-09-26', 91411], ['2024-10-01', 91411], ['2024-10-03', 91411], ['2024-10-08', 91411], ['2024-10-10', 91411] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -93,7 +93,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 0], ['2024-10-01', 0], ['2024-10-03', 0], ['2024-10-08', 0] ]
+		[ ['2024-09-26', 0], ['2024-10-01', 0], ['2024-10-03', 0], ['2024-10-08', 0], ['2024-10-10', 0] ]
 	],
 	[
 		//nome de exibição
@@ -101,7 +101,7 @@ const student = [
 		//nome do usuario (username)
 		'nayramelo',
 		//evolução da pontuação
-		[ ['2024-09-26', 141403], ['2024-10-01', 141403], ['2024-10-03', 143113], ['2024-10-08', 144968] ],
+		[ ['2024-09-26', 141403], ['2024-10-01', 141403], ['2024-10-03', 143113], ['2024-10-08', 144968], ['2024-10-10', 144968] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -109,7 +109,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/boggle-blue.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 0], ['2024-10-03', 1], ['2024-10-08', 1] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 0], ['2024-10-03', 1], ['2024-10-08', 1], ['2024-10-10', 0] ]
 	],
 	[
 		//nome de exibição
@@ -117,7 +117,7 @@ const student = [
 		//nome do usuario (username)
 		'pedrohenrique',
 		//evolução da pontuação
-		[ ['2024-09-26', 12800], ['2024-10-01', 14250], ['2024-10-03', 14250], ['2024-10-08', 14250] ],
+		[ ['2024-09-26', 12800], ['2024-10-01', 14250], ['2024-10-03', 14250], ['2024-10-08', 14250], ['2024-10-10', 14250] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -125,7 +125,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 0] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 0], ['2024-10-10', 0] ]
 	],
 	[
 		//nome de exibição
@@ -133,7 +133,7 @@ const student = [
 		//nome do usuario (username)
 		'rafaelsilva',
 		//evolução da pontuação
-		[ ['2024-09-26', 34685], ['2024-10-01', 35500], ['2024-10-03', 36335], ['2024-10-08', 38305] ],
+		[ ['2024-09-26', 34685], ['2024-10-01', 35500], ['2024-10-03', 36335], ['2024-10-08', 38305], ['2024-10-10', 40365] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -141,7 +141,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1], ['2024-10-10', 1] ]
 	],
 	[
 		//nome de exibição
@@ -149,7 +149,7 @@ const student = [
 		//nome do usuario (username)
 		'ruan',
 		//evolução da pontuação
-		[ ['2024-09-26', 17130], ['2024-10-01', 18375], ['2024-10-03', 18375], ['2024-10-08', 18375] ],
+		[ ['2024-09-26', 17130], ['2024-10-01', 18375], ['2024-10-03', 18375], ['2024-10-08', 18375], ['2024-10-10', 19700] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -157,7 +157,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 0] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 0], ['2024-10-10', 1] ]
 	],
 	[
 		//nome de exibição
@@ -165,7 +165,7 @@ const student = [
 		//nome do usuario (username)
 		'samuelguedes',
 		//evolução da pontuação
-		[ ['2024-09-26', 143883], ['2024-10-01', 143883], ['2024-10-03', 143883], ['2024-10-08', 145683] ],
+		[ ['2024-09-26', 143883], ['2024-10-01', 143883], ['2024-10-03', 143883], ['2024-10-08', 145683], ['2024-10-10', 145683] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -173,7 +173,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/boggle-yellow.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 0], ['2024-10-03', 0], ['2024-10-08', 1] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 0], ['2024-10-03', 0], ['2024-10-08', 1], ['2024-10-10', 0] ]
 	],
 	[
 		//nome de exibição
@@ -181,7 +181,7 @@ const student = [
 		//nome do usuario (username)
 		'thalysonlima',
 		//evolução da pontuação
-		[ ['2024-09-26', 78695], ['2024-10-01', 81145], ['2024-10-03', 83550], ['2024-10-08', 85425] ],
+		[ ['2024-09-26', 78695], ['2024-10-01', 81145], ['2024-10-03', 83550], ['2024-10-08', 85425], ['2024-10-10', 87320] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -189,7 +189,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1], ['2024-10-10', 1] ]
 	],
 	[
 		//nome de exibição
@@ -197,7 +197,7 @@ const student = [
 		//nome do usuario (username)
 		'thaynaralima',
 		//evolução da pontuação
-		[ ['2024-09-26', 69542], ['2024-10-01', 71127], ['2024-10-03', 73142], ['2024-10-08', 73602] ],
+		[ ['2024-09-26', 69542], ['2024-10-01', 71127], ['2024-10-03', 73142], ['2024-10-08', 73602], ['2024-10-10', 74972] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -205,7 +205,7 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/piceratops-seed.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 1], ['2024-10-08', 1], ['2024-10-10', 1] ]
 	],
 	[
 		//nome de exibição
@@ -213,7 +213,7 @@ const student = [
 		//nome do usuario (username)
 		'yanlima',
 		//evolução da pontuação
-		[ ['2024-09-26', 25525], ['2024-10-01', 25920], ['2024-10-03', 25920], ['2024-10-08', 25920] ],
+		[ ['2024-09-26', 25525], ['2024-10-01', 25920], ['2024-10-03', 25920], ['2024-10-08', 25920], ['2024-10-10', 27585] ],
 		//bonus -> data, pontos, motivo, responsavel
 		[ ],
 		//onus (resgate de prêmio) -> data, pontos, descricao-premio, responsavel
@@ -221,6 +221,6 @@ const student = [
 		//icone do usuario
 		'https://cdn.kastatic.org/images/avatars/svg/blobby-green.svg',
 		//presença -> dia, presente:1/faltou:0
-		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 0], ['2024-10-08', 0] ]
+		[ ['2024-09-26', 1], ['2024-10-01', 1], ['2024-10-03', 0], ['2024-10-08', 0], ['2024-10-10', 1] ]
 	]
 ];
